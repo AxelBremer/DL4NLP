24754e04b159f697e9fcfc3aa8cae9ae21c2f9bc
RochelleChoenni <rochelle_800@hotmail.com>
2019-09-16 16:34:31 +0200

    Training works

--- model.py
+++ model.py
@@ -23,11 +23,13 @@ class NN(nn.Module):
                            bidirectional=bidirectional, 
                            dropout=dropout)
         
-        self.fc = nn.Linear(hidden_dim * 2, output_dim)
+        self.fc = nn.Linear(hidden_dim, output_dim)
         
         self.dropout = nn.Dropout(dropout)
+
+        self.sigmoid = nn.Sigmoid()
         
-    def forward(self, text, text_lengths):
+    def forward(self, text):
         
         #text = [sent len, batch size]
         
@@ -35,17 +37,17 @@ class NN(nn.Module):
         
         #embedded = [sent len, batch size, emb dim]
         
-        packed_embedded = nn.utils.rnn.pack_padded_sequence(embedded, text_lengths)
+        #packed_embedded = nn.utils.rnn.pack_padded_sequence(embedded, text_lengths)
         
-        packed_output, (hidden, cell) = self.rnn(packed_embedded)
+        output, (hidden, cell) = self.nn(embedded)
         
-        output, output_lengths = nn.utils.rnn.pad_packed_sequence(packed_output)
+        #output, output_lengths = nn.utils.rnn.pad_packed_sequence(packed_output)
 
         #output = [sent len, batch size, hid dim * num directions]        
         
         #concat the final forward (hidden[-2,:,:]) and backward (hidden[-1,:,:]) hidden layers
         #and apply dropout
         
-        hidden = self.dropout(torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim = 1))
+        # hidden = self.dropout(torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim = 1))
                             
-        return self.fc(hidden)
+        return self.sigmoid(self.fc(self.dropout(output[:,-1,:].squeeze())))
--- train.py
+++ train.py
@@ -11,64 +11,71 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 
-from dataset import ImdbDataset
-from lstm import LSTM
+from imdb_dataset import IMDBDataset
+from model import NN
 
 def train(config):
 
     # Initialize the device which to run the model on
     device = torch.device(config.device)
 
-    # Initialize the model that we are going to use
-    model = LSTM(config.input_dim, config.embed_dim, config.hidden_dim, config.output_dim, config.n_layers, config.bidirectional, config.dropout, 0).to(device) 
-            
-      
+   
     # Initialize the dataset and data loader (note the +1)
-    dataset = ImdbDataset(config.input_length+1)
-    data_loader = DataLoader(dataset, config.batch_size, num_workers=1)
+    dataset = IMDBDataset(train_or_test='train', seq_length=config.seq_length)
+    data_loader = DataLoader(dataset, config.batch_size, shuffle=True, num_workers=1)
+
+     # Initialize the model that we are going to use
+    model = NN(dataset.vocab_size, config.embed_dim, config.hidden_dim, config.output_dim, config.n_layers, config.bidirectional, config.dropout, 0).to(device)
 
     # Setup the loss and optimizer
     criterion = nn.CrossEntropyLoss().to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
+    optimizer = torch.optim.RMSprop(model.parameters()) # lr=config.learning_rate)
     highest = 0 
     save = []
-    for step, (batch_inputs, batch_targets) in enumerate(data_loader):
+    epochs = 0
+
+    print('Starting training...')
+
+    while epochs < config.train_epochs:
 
         t1 = time.time()
 
-        predictions = model(text, text_length).to(device)
+        for step, (batch_inputs, batch_targets) in enumerate(data_loader):
 
-        loss = criterion(predictions, batch_targets.to(device))
-        optimizer.zero_grad()
-        loss.backward()
-        
-        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=config.max_norm)
-        optimizer.step()
+            x = batch_inputs.to(device)
+            y_target = batch_targets.to(device)
 
-        
-        accuracy = float(torch.sum(torch.argmax(predictions, dim=1) == batch_targets.to(device)).item()) / config.batch_size
-        loss = loss.item()
-        
+            predictions = model(batch_inputs).to(device)
+
+
+            loss = criterion(predictions, y_target)
+            optimizer.zero_grad()
+            loss.backward()
+            
+            # torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=config.max_norm)
+            optimizer.step()
+
+            
+            accuracy = (torch.argmax(predictions, dim=1) == batch_targets.to(device)).numpy().mean()
+            loss = loss.item()
+            
+            if step % 100== 0:
+                save.append(accuracy)
+
+
+            if (accuracy > highest):
+                highest = accuracy
+                torch.save(model.state_dict(), 'lstm-model.pt')
+
+
         t2 = time.time()
-        examples_per_second = config.batch_size/float(t2-t1)
-        
-        if step % 10 == 0:
-
-            print("[{}] Train Step {:04d}/{:04d}, Batch Size = {}, Examples/Sec = {:.2f}, "
-                  "Accuracy = {:.2f}, Loss = {:.3f}".format(
-                    datetime.now().strftime("%Y-%m-%d %H:%M"), step,
-                    config.train_steps, config.batch_size, examples_per_second,
-                    accuracy, loss
-            ))
-            save.append(accuracy)
-
-
-        if (accuracy > highest):
-            highest = accuracy
-            torch.save(model.state_dict(), 'lstm-model.pt')
-
-        if step == config.train_steps:
-            break
+        examples_per_second = len(dataset)/float(t2-t1)
+        epochs += 1
+        print("[{}] Train epochs {:04d}/{:04d}, Batch Size = {}, Examples/Sec = {:.2f}, "
+                      "Accuracy = {:.2f}, Loss = {:.3f}".format(
+                        datetime.now().strftime("%Y-%m-%d %H:%M"), epochs,
+                        config.train_epochs, config.batch_size, examples_per_second,
+                        accuracy, loss))
 
     print('Done training.')
     return accuracy, highest, save
@@ -79,16 +86,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # Model params
-    parser.add_argument('--input_dim', type=int, default=100, help='Dimensionality of input sequence')
+    parser.add_argument('--seq_length', type=int, default=30, help='Dimensionality of input sequence')
     parser.add_argument('--embed_dim', type=int, default=100, help='Dimensionality of the embeddings')
-    parser.add_argument('--output_dim', type=int, default=1, help='Dimensionality of output sequence')
-    parser.add_argument('--num_hidden', type=int, default=256, help='Number of hidden units in the model')
+    parser.add_argument('--output_dim', type=int, default=2, help='Dimensionality of output sequence')
+    parser.add_argument('--hidden_dim', type=int, default=256, help='Number of hidden units in the model')
+    parser.add_argument('--n_layers', type=int, default=2, help='Number of hidden units in the model')
     parser.add_argument('--batch_size', type=int, default=128, help='Number of examples to process in a batch')
     parser.add_argument('--learning_rate', type=float, default=0.001, help='Learning rate for Adam')
     parser.add_argument('--dropout', type=float, default=0.5, help='Drop out rate')
-    parser.add_argument('--train_steps', type=int, default=10000, help='Number of training steps')
-    parser.add_argument('--bidirectional', type=bool, default=True)
-    parser.add_argument('--device', type=str, default="cuda:0", help="Training device 'cpu' or 'cuda:0'")
+    parser.add_argument('--train_epochs', type=int, default=50, help='Number of training epochs')
+    parser.add_argument('--bidirectional', type=bool, default=False)
+    parser.add_argument('--device', type=str, default="cpu", help="Training device 'cpu' or 'cuda:0'")
 
     config = parser.parse_args()
 
